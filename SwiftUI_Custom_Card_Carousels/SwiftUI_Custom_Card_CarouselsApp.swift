1dc90dd5bad9722b2a22d4e0f89133b82d45c45f
devingrischow <devingrischow@outlook.com>
2023-11-18 18:54:17 -0600

    Initial Commit

---
+++ SwiftUI_Custom_Card_Carousels/SwiftUI_Custom_Card_CarouselsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Custom_Card_CarouselsApp.swift
+//  SwiftUI_Custom_Card_Carousels
+//
+//  Created by Devin Grischow on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Custom_Card_CarouselsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
